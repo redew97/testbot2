6cb6e36d0061138282488478cf4a0307cc459379
redew97 <40416476+redew97@users.noreply.github.com>
2018-06-21 16:54:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 var HTTPS = require('https');
-var cool = require('cool-ascii-faces');
 
 var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy$/;
+      botRegex = /^\/test$/;
 
+  console.log("request");
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage();
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "FUCK OFF";
 
   options = {
     hostname: 'api.groupme.com',
