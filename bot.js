d50df151d9f6cc81e898c66e3f7b3cfe3be4a0f3
redew97 <40416476+redew97@users.noreply.github.com>
2018-06-21 17:48:08 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,8 +4,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\Yo$/,
-      botRegex = /^\Oy$/;
+      botRegex = /^\Yo$/,/^\Oy$/;
 
   console.log("request");
   if(request.text && botRegex.test(request.text)) {
